a62fba4e2906ad28d5cf0f1367fd442c02e7194c
kolibriof <kolibri380@gmail.com>
2023-02-23 11:46:31 +0100

    todo-list

---
+++ tasks/initial-project/src/data.js
@@ -0,0 +1,235 @@
+import hulu from "./media/hulu_logo.png";
+import bbcStudios from "./media/bbc_studios_logo.png";
+import universals from "./media/universals_logo.png";
+import adobe from "./media/adobe_logo.png";
+import fbIcon from "./media/icons/fb_icon.png";
+import igIcon from "./media/icons/ig_icon.png";
+import twitterIcon from "./media/icons/twitter_icon.png";
+import youtubeIcon from "./media/icons/youtube_icon.png";
+import cursorWelcome from "./media/welcome_background_figures/welcome_section_cursor.png";
+import triangleWelcome from "./media/welcome_background_figures/welcome_section_triangle.png";
+import rectangleWelcome from "./media/welcome_background_figures/welcome_section_rectangle.png";
+import ellipseWelcome from "./media/welcome_background_figures/welcome_section_ellipse.png";
+
+export const photos = [
+  {
+    src: hulu,
+    alt: "company",
+  },
+  {
+    src: bbcStudios,
+    alt: "company",
+  },
+  {
+    src: universals,
+    alt: "company",
+  },
+  {
+    src: adobe,
+    alt: "company",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Product",
+    contents: [
+      {
+        label: "Product",
+        href: "google.com",
+      },
+      {
+        label: "Pricing",
+        href: "google.com",
+      },
+      {
+        label: "Enterprise",
+        href: "google.com",
+      },
+      {
+        label: "Partners",
+        href: "google.com",
+      },
+      {
+        label: "Affiliate",
+        href: "google.com",
+      },
+      {
+        label: "Integrations",
+        href: "google.com",
+      },
+      {
+        label: "Developers",
+        href: "google.com",
+      },
+      {
+        label: "Students",
+        href: "google.com",
+      },
+      {
+        label: "Work OS",
+        href: "google.com",
+      },
+    ],
+  },
+  {
+    title: "Team",
+    contents: [
+      {
+        label: "About us",
+        href: "google.com",
+      },
+      {
+        label: "Contact Us",
+        href: "google.com",
+      },
+      {
+        label: "Careers",
+        href: "google.com",
+      },
+      {
+        label: "Find a Partner",
+        href: "google.com",
+      },
+      {
+        label: "In the News",
+        href: "google.com",
+      },
+    ],
+  },
+  {
+    title: "Solutions",
+    contents: [
+      {
+        label: "Project Management",
+        href: "google.com",
+      },
+      {
+        label: "Marketing",
+        href: "google.com",
+      },
+      {
+        label: "CRM and Sales",
+        href: "google.com",
+      },
+      {
+        label: "Software Development",
+        href: "google.com",
+      },
+      {
+        label: "Constructions",
+        href: "google.com",
+      },
+      {
+        label: "Creative Production",
+        href: "google.com",
+      },
+      {
+        label: "Remote Work",
+        href: "google.com",
+      },
+      {
+        label: "HR",
+        href: "google.com",
+      },
+      {
+        label: "IT",
+        href: "google.com",
+      },
+      {
+        label: "See More Solutions",
+        href: "google.com",
+      },
+    ],
+  },
+  {
+    title: "Resources",
+    contents: [
+      {
+        label: "Knowledge Base",
+        href: "google.com",
+      },
+      {
+        label: "Guides",
+        href: "google.com",
+      },
+      {
+        label: "Daily Webinars",
+        href: "google.com",
+      },
+      {
+        label: "Community",
+        href: "google.com",
+      },
+      {
+        label: "Customer Stories",
+        href: "google.com",
+      },
+      {
+        label: "Templates",
+        href: "google.com",
+      },
+      {
+        label: "Profesionals Services",
+        href: "google.com",
+      },
+      {
+        label: "Video Tutorials",
+        href: "google.com",
+      },
+      {
+        label: "Blog",
+        href: "google.com",
+      },
+      {
+        label: "Podcast",
+        href: "google.com",
+      },
+    ],
+  },
+];
+
+export const footerIcons = [
+  {
+    src: fbIcon,
+    alt: "facebook icon",
+    href: "https://facebook.com",
+  },
+  {
+    src: igIcon,
+    alt: "instagram icon",
+    href: "https://instagram.com",
+  },
+  {
+    src: twitterIcon,
+    alt: "twitter icon",
+    href: "https://twitter.com",
+  },
+  {
+    src: youtubeIcon,
+    alt: "youtube icon",
+    href: "https://youtube.com",
+  },
+];
+export const welcomeBackgroundImages = [
+  {
+    src: cursorWelcome,
+    alt: "cursor welcome",
+    className: "cursor-welcome-section",
+  },
+  {
+    src: ellipseWelcome,
+    alt: "ellipse welcome",
+    className: "ellipse-welcome-section",
+  },
+  {
+    src: triangleWelcome,
+    alt: "triangle welcome",
+    className: "triangle-welcome-section",
+  },
+  {
+    src: rectangleWelcome,
+    alt: "rectangle welcome",
+    className: "rectangle-welcome-section",
+  },
+];
