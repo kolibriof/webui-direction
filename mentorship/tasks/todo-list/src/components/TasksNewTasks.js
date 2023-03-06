08146f2f4c4abf5316fdb5433e19cc454b9eefde
Ozarko <ozarkonv@gmail.com>
2023-03-06 14:20:03 +0200

    Mentor: merge master int broken branch

--- mentorship/tasks/todo-list/src/components/TasksNewTasks.js
+++ mentorship/tasks/todo-list/src/components/TasksNewTasks.js
@@ -1,5 +1,5 @@
 import React from "react";
-import List from "../components/List";
+import List from "./List";
 import "../styles/tasks.css";
 import { IoMdAddCircleOutline } from "react-icons/io";
 import "../styles/list.css";
